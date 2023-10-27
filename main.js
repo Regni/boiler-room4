72e51d578de97776fcc1b2839707f61ce983dbe6
Alexander Kuiper <ak224nx@student.lnu.se>
2023-10-27 10:34:18 +0200

    btn works

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const submitbtn = document.getElementById("submitbtn");
+const form = document.querySelector("form");
 
-submitbtn.addEventListener("click",function(){
+// form.addEventListener("submit", function(event){
+//   event.preventDefault()
+// })
+
+
+submitbtn.addEventListener("click",function(e){
+  e.preventDefault()
   console.log("we have sendedede it")
 })
 
