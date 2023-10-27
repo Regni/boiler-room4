86b8653aca1ebe64c322857e7c38dc319e0461ed
MustafaAltaie <mustafaphoto111@gmail.com>
2023-10-27 11:45:07 +0200

    Mustafa

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const submitbtn = document.getElementById("submitbtn");
 const form = document.querySelector("form");
 
-// form.addEventListener("submit", function(event){
-//   event.preventDefault()
-// })
-
 
 submitbtn.addEventListener("click",function(e){
   e.preventDefault()
   console.log("we have sendedede it")
-})
+});
 
-console.log("heloo")
+document.addEventListener('click', function(evt){
+  if(evt.target.type == 'checkbox' && evt.target.checked == true){
+    const newLi = document.createElement('li');
+    newLi.id = evt.target.id + 'Item';
+    newLi.textContent = evt.target.value;
+    document.getElementById('list').appendChild(newLi);
+  }
+});
